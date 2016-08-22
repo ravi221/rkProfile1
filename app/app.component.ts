a9d1910b27fb1d22a8d30fc7f1f38e880e70bb86
ravi221 <mrravi221@gmail.com>
2016-08-22 11:39:19 +0530

    04-Multilang

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {TranslateService} from 'ng2-translate/ng2-translate';
 
 var themeName = 'default';
 @Component({
   selector: 'my-app',
   templateUrl: '../themes/' + themeName +'/theme.tpl.html',
 })
-export class AppComponent { }
+export class AppComponent implements OnInit { 
+	public langList = [];
+
+	constructor(private translate: TranslateService) {
+        translate.setDefaultLang('en');
+        translate.use('de');
+		translate.use('te');
+    }
+
+
+    ngOnInit(){
+    	this.langList = [
+    		{lang:'en', 'flag':'en.gif' },
+    		{lang:'de', 'flag':'de.gif' }
+			{lang:'te', 'flag':'te.gif' }
+    	];
+    }
+
+    onLangChange(val){
+    	this.translate.use(val);
+    }
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -10,10 +10,15 @@ import { ContactComponent }      from './components/contact/contact.component';
 
 import {routing} from './app.routing';
 
+import {HttpModule} from '@angular/http';
+import {TranslateModule} from 'ng2-translate/ng2-translate';
+
 @NgModule({
   imports:      [ 
   	BrowserModule,
   	routing, 
+	HttpModule,
+    TranslateModule.forRoot()
   ],
   declarations: [ AppComponent, HomeComponent, AboutComponent, ProfileComponent, ContactComponent ],
   bootstrap:    [ AppComponent ]
